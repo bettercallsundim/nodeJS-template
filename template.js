66ab7520d17306c89ad1fe4f673d5e93b0174721
sundim <sunxim@yandex.com>
2022-12-06 18:55:47 +0600

    oki

---
+++ template.js
@@ -0,0 +1,35 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readline() {
+  return inputString[currentLine++];
+}
+
+/////////// YOUR CODE STARTS HERE///////
+
+function main() {
+  const x = readline();
+
+  process.stdout.write("hello world: "); // without auto '\n' (newline)
+  console.log(x);
+}
